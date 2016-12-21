38da99eef3eac733f07e58427afa63ab93f3c3ce
dvdrtrgn <dvdrtrgn@gmail.com>
2016-12-21 14:35:41 -0600

    1) hello world

---
+++ 1/program.js
@@ -0,0 +1,11 @@
+/*
+Write a program that prints the text "HELLO WORLD" to the console (stdout).
+*/
+
+const CLI = process.argv.slice(2);
+
+(function (args) {
+
+  console.log('HELLO WORLD');
+
+}(CLI));
